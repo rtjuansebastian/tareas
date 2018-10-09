b45606f78dfdec081cb93fd169baec0720cfd7d2
Juan Rodriguez <rtjuansebastian@gmail.com>
2018-10-08 21:40:37 -0500

    Metodos show, new y create

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -5,4 +5,28 @@ class TareasController < ApplicationController
 		#select * from tareas
 	end
 
+	def show
+		@tarea = Tarea.find(params[:id])
+		#select * from tareas where id=:id
+	end
+
+	def new
+		@tarea = Tarea.new
+	end
+
+	def create
+		@tarea = Tarea.new(
+			titulo: params[:tarea][:titulo], 
+			descripcion: params[:tarea][:descripcion])
+		@tarea.save						
+		#insert into tareas(titulo,descripcion) values('','')
+		redirect_to controller: "tareas", action: "show", id: @tarea.id
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'bienvenido', to: 'welcome#index'
-  get 'tareas/index'
-
+  get 'tareas', to: "tareas#index"
+  get 'tareas/new', to: "tareas#new"
+  post 'tareas', to: "tareas#create"
+  get 'tareas/:id', to: "tareas#show"  
+  get 'tareas/:id/edit', to: "tareas#edit"
+  delete 'tareas/:id', to: "tareas#destroy"
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
