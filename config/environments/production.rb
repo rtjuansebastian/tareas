a69952f3314a6562381d3f2f649ba2992be813e1
Juan Rodriguez <rtjuansebastian@gmail.com>
2018-10-22 16:38:24 -0500

    configuración de producción para usar servidor smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sitio_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: '',
+    port: ,
+    domain: '',
+    user_name: '',
+    password: '',
+    authentication: '',
+    enable_starttls_auto: 
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
